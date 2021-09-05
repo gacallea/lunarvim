15fce1009eac48a3437fd64d9a06ed3a482d8e74
gacallea <gacallea@users.noreply.github.com>
2021-09-05 03:07:55 +0200

    proper indent line plug

--- lua/moreplugs.lua
+++ lua/moreplugs.lua
@@ -1,12 +1,22 @@
 -- Additional Plugins
 lvim.plugins = {
     {"marko-cerovac/material.nvim"},
-    {"folke/tokyonight.nvim"},
     {
       "folke/trouble.nvim",
       cmd = "TroubleToggle",
     },
-    {"glepnir/indent-guides.nvim"},
+    {
+      "lukas-reineke/indent-blankline.nvim",
+      event = "BufRead",
+      setup = function()
+        vim.g.indentLine_enabled = 1
+        vim.g.indent_blankline_char = "▏"
+        vim.g.indent_blankline_filetype_exclude = {"help", "terminal", "dashboard"}
+        vim.g.indent_blankline_buftype_exclude = {"terminal"}
+        vim.g.indent_blankline_show_trailing_blankline_indent = false
+        vim.g.indent_blankline_show_first_indent_level = false
+      end
+    },
     {"rizzatti/dash.vim"},
     {"lewis6991/spellsitter.nvim"},
 }
