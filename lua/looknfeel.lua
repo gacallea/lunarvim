b4a8af59b186fb34854db11f8948b295e4e6b2e0
gacallea <gacallea@users.noreply.github.com>
2022-01-30 10:04:38 +0100

    fixed material new theme req

--- lua/looknfeel.lua
+++ lua/looknfeel.lua
@@ -1,30 +1,48 @@
 -- Material Theme
-vim.g.material_style = "palenight"
+-- vim.g.material_style = "darker"
+vim.g.material_style = "lighter"
+-- vim.g.material_style = "oceanic"
+-- vim.g.material_style = "palenight"
+-- vim.g.material_style = "deep ocean"
 
 require('material').setup({
-	contrast = true, -- Enable contrast for sidebars, floating windows and popup menus like Nvim-Tree
-	borders = true, -- Enable borders between verticaly split windows
+
+	contrast = {
+		sidebars = false, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+		floating_windows = false, -- Enable contrast for floating windows
+		line_numbers = false, -- Enable contrast background for line numbers
+		sign_column = false, -- Enable contrast background for the sign column
+		cursor_line = false, -- Enable darker background for the cursor line
+		non_current_windows = false, -- Enable darker background for non-current windows
+		popup_menu = false, -- Enable lighter background for the popup menu
+	},
+
 	italics = {
-		comments = true, -- Enable italic comments
+		comments = false, -- Enable italic comments
 		keywords = false, -- Enable italic keywords
 		functions = false, -- Enable italic functions
 		strings = false, -- Enable italic strings
 		variables = false -- Enable italic variables
 	},
-	contrast_windows = { -- Specify which windows get the contrasted (darker) background
+
+	contrast_filetypes = { -- Specify which filetypes get the contrasted (darker) background
 		"terminal", -- Darker terminal background
 		"packer", -- Darker packer background
 		"qf" -- Darker qf list background
 	},
-	text_contrast = {
+
+	high_visibility = {
 		lighter = false, -- Enable higher contrast text for lighter style
-		darker = true -- Enable higher contrast text for darker style
+		darker = false -- Enable higher contrast text for darker style
 	},
+
 	disable = {
+		borders = false, -- Disable borders between verticaly split windows
 		background = false, -- Prevent the theme from setting the background (NeoVim then uses your teminal background)
 		term_colors = false, -- Prevent the theme from setting terminal colors
 		eob_lines = false -- Hide the end-of-buffer lines
 	},
+
 	custom_highlights = {} -- Overwrite highlights with your own
 })
 lvim.colorscheme = "material"
