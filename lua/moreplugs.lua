d613b989919c5587b2ffaaa2a66e8354530c1442
gacallea <gacallea@users.noreply.github.com>
2022-04-06 17:11:47 +0200

    minor changes

--- lua/moreplugs.lua
+++ lua/moreplugs.lua
@@ -31,7 +31,7 @@ lvim.plugins = {
       vim.g.indent_blankline_char = "▏"
       vim.g.indent_blankline_filetype_exclude = {"help", "terminal", "dashboard"}
       vim.g.indent_blankline_buftype_exclude = {"terminal"}
-      vim.g.indent_blankline_show_trailing_blankline_indent = false
+      vim.g.indent_blankline_show_trailing_blankline_indent = true
       vim.g.indent_blankline_show_first_indent_level = false
     end
   },
@@ -55,6 +55,9 @@ lvim.plugins = {
     requires = "nvim-lua/plenary.nvim",
   },
   {
+    "kdheepak/lazygit.nvim",
+  },
+  {
     "f-person/git-blame.nvim",
     event = "BufRead",
     config = function()
@@ -74,7 +77,6 @@ lvim.plugins = {
   },
   {
     "windwp/nvim-ts-autotag",
-    event = "InsertEnter",
     config = function()
       require("nvim-ts-autotag").setup()
     end,
